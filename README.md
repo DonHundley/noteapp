f6c4403f7ebbcc0aa200559a2663c2f07e18402f
DonHundley <112941023+DonHundley@users.noreply.github.com>
2024-03-06 13:59:13 +0100

    init w/ dockerized db

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # noteapp
+
+An application built with the idea of storing notes from audio commands for Full-Stack AI project.
+
+This application will utilize Docker, an Angular front end, websockets for the backend, and MySQL for the database. 
+
  
--- backend/api/Program.cs
+++ backend/api/Program.cs
@@ -1,23 +1,59 @@
+using Serilog;
+using System;
+using System.Text.Json;
+using api;
 using Fleck;
+using lib;
+using MySql.Data.MySqlClient;
 
 var app = await ApiStartup.StartApi();
 app.Run();
 
-public static class ApiStartup
+namespace api
 {
-    public static async Task<WebApplication> StartApi()
+    public static class ApiStartup
     {
-        EnvSetup.SetDefaultEnvVariables();
-
-        Log.Information(JsonSerializer.Serialize(Environment.GetEnvironmentVariables(), new JsonSerializerOptions
+        public static async Task<WebApplication> StartApi()
         {
-            WriteIndented = true
-        }));
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console(
+                    outputTemplate: "\n{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level}] {Message}{NewLine}{Exception}\n")
+                .CreateLogger();
 
-        var builder = WebApplication.CreateBuilder();
 
-        
-    }
+            Log.Information(JsonSerializer.Serialize(Environment.GetEnvironmentVariables(), new JsonSerializerOptions
+            {
+                WriteIndented = true
+            }));
 
-    
+            var builder = WebApplication.CreateBuilder();
+
+            string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+            
+            builder.Services.AddTransient<MySqlConnection>(_ => new MySqlConnection(connectionString));
+
+            var app = builder.Build();
+            
+            
+            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? throw new Exception("Not found");
+            if (string.IsNullOrEmpty(env))
+                throw new Exception("Must have ASPNETCORE_ENVIRONMENT");
+
+
+            var server = new WebSocketServer("ws://0.0.0.0:8181");
+
+            server.RestartAfterListenError = true;
+            
+            server.Start(socket =>
+            {
+                socket.OnOpen = () => { Console.WriteLine("Socket opened"); };
+                socket.OnClose = () => { Console.WriteLine("Socket closed"); };
+                socket.OnMessage = message => { Console.WriteLine($"Received message: {message}"); };
+            });
+
+            return app;
+        }
+    }   
 }
+
+
